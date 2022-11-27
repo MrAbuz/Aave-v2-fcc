67f8b6639662514b097f71c1aa52093789dfbf87
MrAbuz <miguelmrosario@gmail.com>
2022-11-27 01:56:27 +0000

    27/11/2022

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -38,9 +38,30 @@ async function main() {
         lendingPool,
         deployer
     )
-
     //Now we wanna know what's the conversion rate on DAI is? How much DAI can I borrow with "availableBorrowsETH"?
     //The borrow function takes the amount of borrow denominated in the asset we wanna borrow I must guess
+    //Keep in mind that this "daiPrice" is DAI/ETH (not ETH/DAI), which returns 828957654412212 which is 0.000828957654412212 because it only has 15 digits (below 18 digits it has decimal cases)
+    //This price feed returns with 18 decimals because its DAI/ETH (we can call .decimals of AggregatorV3Interface)
+    //So 1 / 0.000828957654412212 is 1206 which is the eth price
+    //1 / 0.000828957654412212 makes it ETH/DAI
+    //need to understand the difference between toString() and toNumber()
+
+    const daiPrice = await getDaiPrice()
+
+    const amountDaiToBorrow =
+        availableBorrowsETH.toString() * 0.95 * (1 / daiPrice.toNumber())
+    console.log(`You can borrow:${amountDaiToBorrow} DAI`)
+
+    //this gives us the amount of eth in dai: 18.9 DAI (or 18.9 usd). (*0.95 is just to not borrow the full amount that we can and be near limit)
+    //but we want this amount in wei ("the dai token has 18 decimal places, similar to ethereum, so we need that amount in wei")
+    //in reality we ask for 18909289173660690000 wei (or wei version for DAI) because the erc20 has 18 decimal places (which visually for the users is 18.9 DAI)
+    //for what I understand so far, when dealing with ERC20s I should always use them in wei considering that the ERC20 Im using has 18 decimal places.
+    //(to remember: this wei was created because solidity cant deal with decimal places, so it considers the number in WEI 18 decimal cases bigger so that you can
+    //transact 0.001 because in reality it is 1000000000000000 wei and like this it can deal with super low numbers and do math as if they are whole numbers, then
+    //visually the wallets and UI presents in ETH. Super smart, also calling it in WEI gives the perfect solution)
+    const amountDaiToBorrowWei = ethers.utils.parseEther(
+        amountDaiToBorrow.toString()
+    )
 }
 //Now we wanna start interacting with the aave protocol v2:
 //abi, address
@@ -102,16 +123,30 @@ async function getBorrowUserData(lendingPool, account) {
     console.log(`You can borrow ${availableBorrowsETH} worth of ETH.`)
 
     return { availableBorrowsETH, totalDebtETH } //and need the {} to return more than 1 variable
-    //good function to learn how to work with getting the return variables/functions from a function
+    //(attention) good function to learn how to work with getting the return variables/functions from a function
 }
 
 async function getDaiPrice() {
-    //function to know the dai value of x amount of ETH
+    //nice function to convert the price of any asset (here its to know the dai value of an amount of ETH)
     //Aave has its own function that we can use to know the price conversion and it uses Chainlink too (******)
-    //but since we know how to use the chainlink directly that's what we're gonna do (because the aave market checks from the same chainlink aggregator)
+    //but since we know how to use the chainlink directly that's what we're gonna do (because aave gets it from the chainlink aggregator aswell)
     //got the chainlink aggregatorv3interface from patrick's github (*******) but we could get from chainlink github/docs or import from chainlink npm aswell. Nice!
     //Remember: if I search for chainlink npm, and yarn add --dev that npm, it'll store all the files in node_modules and then I can create a sol file that
     //          imports from one of those folders, and it would be the same as copy pasting the code to a sol file! a good solution
+    //Dai/eth price feed address: "0x773616E4d11A78F511299002da57A0a94577F1f4" from chainlink website with pricefeed addresses (#). This should be in helper-hardhat-config.js
+
+    const daiEthPriceFeed = await ethers.getContractAt(
+        //we don't need to connect this one to the deployer because we're not gonna be sending any transaction, we're just gonna be reading from this contract
+        //to read we don't need a signer
+        "AggregatorV3Interface",
+        "0x773616E4d11A78F511299002da57A0a94577F1f4"
+    )
+
+    const price = (await daiEthPriceFeed.latestRoundData())[1]
+    //(attention) This latestRoundData() returns a lot of variables and we just want the 2nd one (index 1), so we can do it like this. Wrap with () and add [1]
+    //in the end representing the index. So nice!
+    console.log(`The DAI/ETH price is ${price.toString()}`)
+    return price
 }
 
 main()
@@ -129,6 +164,7 @@ main()
 //https://github.com/PatrickAlphaC/hardhat-defi-fcc/blob/main/contracts/interfaces/IERC20.sol (****) got the erc20 interface from patrick github but I might find it from openzepeling or something I think
 //https://docs.aave.com/developers/v/2.0/the-core-protocol/lendingpool (*****) -> ctrl f "getUserAccountData"
 //https://docs.aave.com/developers/v/2.0/the-core-protocol/price-oracle (******)
+//https://docs.chain.link/data-feeds/price-feeds/addresses (#)
 
 //Attention:
 //If I add some interface and it is importing some files from local places ("./"), search for its npm, yarn add --dev that npm and then change the location that its
@@ -145,4 +181,4 @@ main()
 //to the node modules. So we had 2 artifacts for the same name "IlendingPoolAddressesProvider.sol" which was throwing up an error. So Patrick said that since
 //we already have that one "IlendingPoolAddressesProvider.sol" in the node modules that is being compiled, we can delete ours because they are the same.
 
-//19:52:30
+//20:08:41
